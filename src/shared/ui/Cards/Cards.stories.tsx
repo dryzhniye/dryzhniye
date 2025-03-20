28b3b38673e94940762e32f681a53f4e2840d3de
Kirill <kirill0392@gmail.com>
2025-03-20 20:15:57 +0300

    sc-18 feat: add Cards component, module, story

---
+++ src/shared/ui/Cards/Cards.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+import '../../../app/globals.css'
+import Cards from './Cards'
+
+const meta = {
+  title: 'Cards',
+  component: Cards,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Cards>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Story = {
+  args: {
+    children: 'Hello world!',
+  },
+}
--- src/shared/ui/Cards/Cards.tsx
+++ src/shared/ui/Cards/Cards.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import type { ReactNode } from 'react'
+import s from './Cards.module.scss'
 
-export default function Cards() {
+type Type = {
+/**
+ * принимает стили
+ */
+  className?: string
+  /**
+ * принимает различные childrens для компонента
+ */
+  children: ReactNode
+}
+
+/**
+* universal cards component
+ */
+export default function Cards({ className, children, ...rest }: Type) {
   return (
-    <div>Cards</div>
+    <div className={className ? `${className}` : `${s.card}`} {...rest}>
+      {children}
+    </div>
   )
 }
