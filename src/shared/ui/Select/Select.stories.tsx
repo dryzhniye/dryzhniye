62cc52c0fbb0b4a99b918f1b451026c06bba8941
Vitaliy <egoshinvit@gmail.com>
2025-03-22 17:12:23 +0700

    init: create select

---
+++ src/shared/ui/Select/Select.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Select } from '@/shared/ui/Select/Select'
+
+const meta = {
+  component: Select,
+  decorators: [(Story) => (
+    <div style={{ backgroundColor: 'black', padding: '20px', minHeight: '100vh' }}>
+      <Story />
+    </div>
+  )],
+} satisfies Meta<typeof Select>
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    options: ['first', 'second']
+  },
+}
--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import styles from './Select.module.css'
+import Image from 'next/image'
 
-export default function Select() {
+type Props = {
+  placeholder?: string
+  disabled?: boolean
+  options: string[]
+}
+
+export const Select = ({placeholder = "Select-box", disabled, options}: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(placeholder);
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
   return (
-    <div>Select</div>
-  )
+    <div className={styles.selectBox}>
+      <div
+        className={`${styles.selectBoxHeader} ${isOpen ? styles.open : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>{selectedOption}</span>
+        {isOpen ? <Image src={'arrow2.svg'} alt={'arrow'} width="15" height="8"/> : <Image src={'arrow1.svg'} alt={'arrow'} width="15" height="8"/>}
+      </div>
+      {isOpen && (
+        <ul className={styles.selectBoxList}>
+          {options.map((option, index) => (
+            <li
+              key={index}
+              className={styles.selectBoxItem}
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
