1562134e450ae3a2d9cb6e4998f91ff7cd28308e
Lena <lena3005@gmail.com>
2025-03-22 14:03:32 +0600

    fix

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { InputHTMLAttributes } from 'react'
+import s from './Input.module.scss'
 
-export default function Input() {
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string
+  placeholder?: string
+  value?: string
+  type?: 'search' | 'email'
+  disabled?: boolean
+  error?: string
+  icon?: React.ReactNode
+  iconPosition?: 'start' | 'end'
+  onEnterPress?: () => void
+}
+
+const Input: React.FC<InputProps> = ({
+  label,
+  error,
+  disabled,
+  className,
+  placeholder,
+  icon,
+  iconPosition = 'start',
+  // onEnterPress,
+  ...res
+}) => {
+  // const onPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+  //   if (e.key === 'Enter' && onEnterPress) {
+  //     onEnterPress()
+  //   }
+  // }
   return (
-    <div>Input</div>
+    <div
+      className={
+        s.inputContainer +
+        (className ? ' ' + className : '') +
+        (disabled ? ' ' + s.disabled : '') +
+        (error ? ' ' + s.error : '')
+      }
+    >
+      {label && <label className={s.label}>{label}</label>}
+
+      <div className={s.inputWrapper}>
+        {icon && iconPosition === 'start' && <div className={s.icon}>{icon}</div>}
+
+        <input
+          {...res}
+          placeholder={placeholder}
+          disabled={disabled}
+          // onKeyDown={onPressHandler}
+          className={
+            s.input + (error ? ' ' + s.inputError : '') + (disabled ? ' ' + s.inputDisabled : '')
+          }
+        />
+
+        {icon && iconPosition === 'end' && <div className={s.icon}>{icon}</div>}
+      </div>
+
+      {error && <span className={s.errorText}>{error}</span>}
+    </div>
   )
 }
+
+export default Input
