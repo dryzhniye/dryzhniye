831d7cce602fb8cdd2d3cf8422ef2fccb5e7e50e
Lena <lena3005@gmail.com>
2025-03-23 23:23:12 +0600

    add input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,34 +1,46 @@
-import React, { InputHTMLAttributes } from 'react'
+import React, { useState, InputHTMLAttributes, KeyboardEvent, ChangeEvent } from 'react'
 import s from './Input.module.scss'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   placeholder?: string
   value?: string
-  type?: 'search' | 'email'
   disabled?: boolean
   error?: string
   icon?: React.ReactNode
   iconPosition?: 'start' | 'end'
-  onEnterPress?: () => void
+  onEnterPress?: (value: string) => void
 }
 
 const Input: React.FC<InputProps> = ({
   label,
-  error,
+  error: initialError,
   disabled,
   className,
   placeholder,
   icon,
-  iconPosition = 'start',
-  // onEnterPress,
+  iconPosition,
+  onEnterPress,
+  onChange,
+  value,
   ...res
 }) => {
-  // const onPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-  //   if (e.key === 'Enter' && onEnterPress) {
-  //     onEnterPress()
-  //   }
-  // }
+  const [error, setError] = useState(initialError)
+  const [inputValue, setInputValue] = useState(value || '')
+
+  const onPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && onEnterPress) {
+      onEnterPress(inputValue)
+      setInputValue('')
+    }
+  }
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+    setError('')
+    onChange?.(e)
+  }
+
   return (
     <div
       className={
@@ -41,19 +53,23 @@ const Input: React.FC<InputProps> = ({
       {label && <label className={s.label}>{label}</label>}
 
       <div className={s.inputWrapper}>
-        {icon && iconPosition === 'start' && <div className={s.icon}>{icon}</div>}
+        {icon && iconPosition === 'start' && (
+          <div className={`${s.icon} ${s.iconStart}`}>{icon}</div>
+        )}
 
         <input
           {...res}
           placeholder={placeholder}
           disabled={disabled}
-          // onKeyDown={onPressHandler}
-          className={
-            s.input + (error ? ' ' + s.inputError : '') + (disabled ? ' ' + s.inputDisabled : '')
-          }
+          onKeyDown={onPressHandler}
+          onChange={onChangeHandler}
+          className={`${s.input} 
+      ${error ? ' ' + s.inputError : ''} 
+      ${disabled ? ' ' + s.inputDisabled : ''} 
+      ${icon && iconPosition === 'start' ? ' ' + s.withIconStart : ''} 
+      ${icon && iconPosition === 'end' ? ' ' + s.withIconEnd : ''}`}
         />
-
-        {icon && iconPosition === 'end' && <div className={s.icon}>{icon}</div>}
+        {icon && iconPosition === 'end' && <div className={`${s.icon} ${s.iconEnd}`}>{icon}</div>}
       </div>
 
       {error && <span className={s.errorText}>{error}</span>}
