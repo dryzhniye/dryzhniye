001c2b96e80158fe01b14a818d1a26f87e29cdbd
incubator_alex <aleksei.tselikin@gmail.com>
2025-03-25 10:14:22 +0300

    fix storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 import { Typography } from '@/shared/ui/Typography/Typography'
 import s from './page.module.scss'
-import { DatePicker } from '@/shared/ui/DatePicker'
+// import { DatePicker } from '@/shared/ui/DatePicker'
 
 export default function Home() {
   return (
     <>
       <span className={s.spanColor}>test</span>
-      <Typography variant={'medium-text-14'} font={'roboto'} as={'div'} className={s.spanColor}>Hello World</Typography>
-      <Typography variant={'regular-text-14'} font={'roboto'} as={'div'} className={s.spanColor}>Hello
-        World</Typography>
-      <Typography asChild={true}><h1>hello world</h1></Typography>
-      <div style={{ display: 'flex', gap: '4px' }}><DatePicker mode={'single'} />
-        <DatePicker /></div>
+      <Typography variant={'medium-text-14'} font={'roboto'} as={'div'} className={s.spanColor}>
+        Hello World
+      </Typography>
+      <Typography variant={'regular-text-14'} font={'roboto'} as={'div'} className={s.spanColor}>
+        Hello World
+      </Typography>
+      <Typography asChild={true}>
+        <h1>hello world</h1>
+      </Typography>
+
+      {/*<div style={{ display: 'flex', gap: '4px' }}>*/}
+      {/*  <DatePicker mode={'single'} />*/}
+      {/*  <DatePicker />*/}
+      {/*</div>*/}
     </>
   )
 }
