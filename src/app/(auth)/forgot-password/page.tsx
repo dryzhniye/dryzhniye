09f407861586fe7a476d1fd3b6c6b489006fa9bf
Vitaliy <egoshinvit@gmail.com>
2025-03-30 13:01:00 +0700

    init: add forgot-password page

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+import { Header } from '@/shared/ui/Header/Header'
+import s from './forgot-password.module.scss'
+import Input from '@/shared/ui/Input/Input'
+import { Button } from '@/shared/ui/Button/Button'
+import Recaptcha from '@/shared/ui/Recaptcha/Recaptcha'
+
 export default function ForgotPassword() {
-    return (
-      <div>
-        <h1>ForgotPassword Page</h1>
+  return (
+    <div>
+      <Header isLoggedIn={true} title="Inctagram" />
+      <div className={s.block}>
+        <h1 className={s.title}>Forgot Password</h1>
+        <Input label={'Email'} placeholder={'Epam@epam.com'} />
+        <Button title={'Send Link'} />
+        <Button title={'Back to Sign In'} />
+        <Recaptcha />
       </div>
-    );
-  }
+    </div>
+  )
+}
