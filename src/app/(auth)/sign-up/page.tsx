c653d1f8e638c72688a89b76d279059beb6f5cc8
incubator_alex <aleksei.tselikin@gmail.com>
2025-03-31 13:23:03 +0300

    chore: add sign-up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,25 @@
 import { Header } from '@/shared/ui/Header/Header'
-import { Sidebar } from '@/shared/ui/Sidebar/Sidebar'
+import styles from './signUp.module.scss'
+import Input from '@/shared/ui/Input/Input'
 
 export default function LoginPage() {
   return (
     <div>
       <Header isLoggedIn={false} />
-      <Sidebar />
+      <form className={styles.block}>
+        <h1 style={{ color: 'var(--light-100)', fontSize: '20px' }}>Sign Up</h1>
+        <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+          <a href="#">
+            <img src="/google.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/github.svg" alt="" />
+          </a>
+        </div>
+        <Input label={'Username'} placeholder={'Username'} />
+        <Input label={'Email'} placeholder={'email'} />
+        <Input label={'Password'} placeholder={'Password'} />
+      </form>
     </div>
   )
 }
