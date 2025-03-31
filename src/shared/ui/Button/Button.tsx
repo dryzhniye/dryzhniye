e6d002255a0a78feb6f838f10204e60c22518670
incubator_alex <aleksei.tselikin@gmail.com>
2025-03-31 11:13:25 +0300

    fix: fix header

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,18 +1,34 @@
-import React, {ComponentPropsWithoutRef, ElementType} from 'react'
+import React, { ComponentPropsWithoutRef, ElementType } from 'react'
 import s from './Button.module.scss'
 
 export type Props<T extends ElementType = 'button'> = {
   asChild?: T
-  variant?: "primary" | "secondary" | "outlined" | "link" | "disabled"
-  title?: string
+  variant?: 'primary' | 'secondary' | 'outlined' | 'link' | 'disabled'
+  title: string
   fullWidth?: boolean
   width?: string
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: Props<T>) => {
-  const {variant = "primary", fullWidth, title, className, asChild: Component = 'button', width, ...rest} = props
+  const {
+    variant = 'primary',
+    fullWidth,
+    title,
+    className,
+    asChild: Component = 'button',
+    width,
+    ...rest
+  } = props
 
   return (
-      <Component {...rest} className={ `${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className} ${s.button} `} style={{width: width}}>{title}</Component>
+    <Component
+      {...rest}
+      className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className} ${s.button} `}
+      style={{ width: width }}
+    >
+      {title}
+    </Component>
+
+    // todo: ссылка должна быть на всю ширину
   )
 }
