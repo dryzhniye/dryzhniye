a47610c4affec68ab377ca8c6c986a91f8aef8ea
Vitaliy <egoshinvit@gmail.com>
2025-03-31 18:29:31 +0700

    refactor: add correct validation and redirect

--- src/app/auth/recovery/page.tsx
+++ src/app/auth/recovery/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import { Button } from '@/shared/ui/Button/Button'
 import { redirect, useSearchParams } from 'next/navigation'
 import { useCheckRecoveryCodeMutation, useCreateNewPasswordMutation } from '@/app/auth/api/authApi'
+import { useRouter } from 'next/navigation'
 
 type createPasswordArgs = {
   password1: string
@@ -27,6 +28,8 @@ export default function Page() {
     defaultValues: { password1: '', password2: '' },
   })
 
+  const router = useRouter()
+
   const [checkRecoveryCode] = useCheckRecoveryCodeMutation()
   const [createNewPassword] = useCreateNewPasswordMutation()
 
@@ -39,17 +42,17 @@ export default function Page() {
       checkRecoveryCode(code).unwrap().catch(() => {
         redirect('/auth/recovery-resending?email=' + email)
       })
+    } else {
+      redirect('/auth/sign-in')
     }
   }, [])
 
   const onSubmit: SubmitHandler<createPasswordArgs> = async (data) => {
     if (code) {
       try {
-        debugger
-        await createNewPassword({ newPassword: data.password1, recoveryCode: `${code}` }).unwrap()
-        redirect('/auth/sign-in')
-      }
-      catch (error) {
+        await createNewPassword({ newPassword: data.password1, recoveryCode: code }).unwrap()
+        router.push('/auth/sign-in')
+      } catch (error) {
         const apiError = (error as {
           data?: {
             statusCode: number;
@@ -57,19 +60,20 @@ export default function Page() {
             error: string;
           }
         }).data
-
+        reset()
         setError('password1', {
           type: 'manual',
           message: apiError?.messages[0].message,
         })
-        reset()
       }
+    }
+    else {
+      redirect('/auth/sign-in')
     }
   }
 
   //todo fix images issues
   //todo fix load content before initialize
-  //todo fix validate
   //todo add ssr for 2 todo
 
   return (
@@ -92,7 +96,7 @@ export default function Page() {
             message: 'Max 20 characters',
           },
           pattern: {
-            value: /^(?=.*[A-Z])[A-Za-z\d!@#$%^&*()_+]{6,20}$/,
+            value: /^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-.,])[A-Za-z\d!@#$%^&*()_+\-.,]{6,20}$/,
             message: 'Must contain: 1 uppercase letter, Latin characters, numbers, or special symbols',
           },
         })} />
@@ -103,7 +107,7 @@ export default function Page() {
                error={errors.password2?.message} {...register('password2', {
           required: 'Password is required',
           validate: (value) =>
-            value === watch('password1') || 'Passwords do not match',
+            value === watch('password1') || '{Passwords must match',
         })} />
         <p className={s.label + ' ' + s.password}>Your password must be between 6 and 20 characters</p>
         <Button title={'Create new password'} width={'100%'} className={s.button + ' ' + s.password} type={'submit'}
