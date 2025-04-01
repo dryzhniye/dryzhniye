ec7fb2a1541f8f59e5ca2161e38b4360f3d3ed8f
Kirill <kirill0392@gmail.com>
2025-04-01 20:42:32 +0300

    UC-20 feat: add onSubmit

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -14,16 +14,51 @@ type LoginArgs = {
 }
 
 export default function LoginPage() {
+
+    // const [logIn] = useResetPasswordMutation()
+
   const {
     register,
     handleSubmit,
-    reset,
     setError,
-    formState: { errors, isValid, isDirty },
+    formState: { errors },
   } = useForm<LoginArgs>({
     mode: 'onChange',
     defaultValues: { email: '', password: '' },
   })
+
+  const onSubmit = async (data: LoginArgs) => {
+    if (!data.email || !data.password) {
+      setError('password', {
+        type: 'manual',
+        message: 'The email or password are incorrect. Try again please',
+      })
+    } else {
+      console.log('Form submitted', data)
+    }
+
+    try {
+      // await logIn({
+      //   email: data.email,
+      //   password: data.password,
+      // }).unwrap()
+
+
+    } catch (error) {
+      const apiError = (error as {
+        data?: {
+          statusCode: number;
+          messages: Array<{ message: string; field: string }>;
+          error: string;
+        }
+      }).data
+
+      setError('email', {
+        type: 'manual',
+        message: apiError?.messages[0].message,
+      })
+    }
+  }
 
   return (
     <>
@@ -42,9 +77,10 @@ export default function LoginPage() {
         </div>
 
         <Input
-          label={'Email'}
-          placeholder={'Epam@epam.com'}
-          width={'100%'}
+          label="Email"
+          placeholder="Epam@epam.com"
+          width="100%"
+          error={errors.email?.message}
           {...register('email', {
             required: 'Email is required',
             pattern: {
@@ -53,18 +89,37 @@ export default function LoginPage() {
             },
           })}
         />
+
         <Input
-          label={'Password'}
-          placeholder={'**********'}
-          type={'password'}
-          width={'100%'}
+          label="Password"
+          placeholder="**********"
+          type="password"
+          width="100%"
           className={s.input}
+          error={errors.password?.message}
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 6,
+              message: 'Password must be at least 6 characters long',
+            },
+            maxLength: {
+              value: 20,
+              message: 'Password must be at most 20 characters long',
+            },
+          })}
         />
 
-        <Typography className={s.forgot}>Forgot Password</Typography>
-        <Button title="Sign In" width={'100%'} />
+        <Button
+          className={s.forgot}
+          variant={'link'}
+          asChild={'a'}
+          title="Forgot Password"
+          href={'/forgot-password'}
+        />
+        <Button title="Sign In" width={'100%'} onClick={handleSubmit(onSubmit)} />
         <Typography className={s.account}>Don’t have an account?</Typography>
-        <Button title={'Sign Up'} variant={'link'} asChild={'a'} width={'100%'} />
+        <Button title={'Sign Up'} variant={'link'} asChild={'a'} width={'100%'} href={'/sign-up'} />
       </Cards>
     </>
   )
