a445968c760568af6727bf120b5d23ffdbe7b600
Vitaliy <egoshinvit@gmail.com>
2025-04-06 21:41:14 +0700

    hot fix build

--- src/app/constants/github-auth-handler.ts
+++ src/app/constants/github-auth-handler.ts
@@ -1,9 +1,9 @@
 export const handleGithubAuth = () => {
-  const redirectUrl = 'http://localhost:3000/auth/callback'
+  const redirectUrl = 'https://dryzhniye.ru/auth/callback'
   // const redirectUrl = 'http://localhost:3000/auth/callback/github'
 
   // const loginUrl = `https://dryzhniye.ru/api/v1/auth/github/login?redirect_url=${encodeURIComponent(redirectUrl)}`
-  const loginUrl = `https://inctagram.work/api/v1/auth/github/login?redirect_url=${encodeURIComponent(redirectUrl)}`
+  const loginUrl = `https://dryzhniye.ru/api/v1/auth/github/login?redirect_url=${encodeURIComponent(redirectUrl)}`
   // const loginUrl = `https://google.com`
 
   window.location.href = loginUrl
