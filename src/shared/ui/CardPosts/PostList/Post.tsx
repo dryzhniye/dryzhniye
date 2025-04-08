72f6db9de3cdd9b93ae6234eea5606e8b15ec3e9
Lena <lena3005@gmail.com>
2025-04-08 20:34:52 +0600

    add Post

--- src/shared/ui/CardPosts/PostList/Post.tsx
+++ src/shared/ui/CardPosts/PostList/Post.tsx
@@ -1,32 +1,67 @@
 'use client'
 
 import Image from 'next/image'
+import { useState } from 'react'
 import s from './Post.module.scss'
 
 type PostProps = {
+  id: number
   imageUrl: string
   description: string
   likes: number
   date: string
+  avatarOwner: string
+  owner: {
+    firstName: string
+    lastName: string
+  }
 }
 
-const Post: React.FC<PostProps> = ({ imageUrl, description, likes, date }) => {
+const Post: React.FC<PostProps> = ({
+  id,
+  imageUrl,
+  description,
+  likes,
+  date,
+  avatarOwner,
+  owner,
+}) => {
+  const [isLiked, setIsLiked] = useState(false)
+
+  const handleLikeClick = () => {
+    setIsLiked(prev => !prev)
+  }
+
   return (
     <div className={s.postContent}>
-      {/* Изображение поста */}
-      <Image src={imageUrl} alt="Post image" width={720} height={720} className={s.image} />
+      <div className={s.header}>
+        <Image src={avatarOwner} alt="User avatar" width={20} height={20} className={s.avatar} />
+      </div>
 
-      {/* Описание, лайки, время */}
       <div className={s.content}>
-        <p>{description}</p>
-        <div className={s.timestamp}>{date}</div>
-        <span>Like: {likes}</span>
-        <span>Answer</span>
-        <div className={s.actions}>
-          <button>
-            <Image src="/heart.svg" alt="Like" width={20} height={20} />
-          </button>
+        <p className={s.description}>
+          {' '}
+          <span className={s.userName}>
+            {owner.firstName} {owner.lastName}
+          </span>
+          {description}
+        </p>
+        <div className={s.info}>
+          <div className={s.timestamp}>{date}</div>
+          <span className={s.description}>Likes: {likes}</span>
+          <button className={s.description}>Answer</button>
         </div>
+      </div>
+
+      <div className={s.actions}>
+        <button onClick={handleLikeClick}>
+          <Image
+            src={isLiked ? '/heart.svg' : '/heart-outline.svg'}
+            alt="Like"
+            width={20}
+            height={20}
+          />
+        </button>
       </div>
     </div>
   )
--- src/shared/ui/CardPosts/PostList/PostList.tsx
+++ src/shared/ui/CardPosts/PostList/PostList.tsx
@@ -1,41 +1,114 @@
 'use client'
 
 import Post from '@/shared/ui/CardPosts/PostList/Post'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import s from './PostList.module.scss'
 
-// Пример данных (в реальном приложении данные будут загружаться через API)
 const mockPosts = [
   {
     id: 1,
-    imageUrl: 'https://example.com/image.jpg',
+    userName: 'Alex',
     description:
-      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    username: 'URL_PROFILE',
-    comments: [
+      'description URLProfiele Lorem ipsum dolor sit amet, ' +
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+      'ut labore et dolore magna aliqua.',
+    location: 'location',
+    images: [
       {
-        user: 'URL_PROFILE',
-        text: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        timestamp: '2 Hours ago',
-        answers: [
-          {
-            user: 'URL_PROFILE',
-            text: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            timestamp: '2 Hours ago',
-          },
-        ],
+        url: '/google.svg',
+        width: 300,
+        height: 300,
+        fileSize: 300,
+        createdAt: '2025-04-02T06:32:49.111Z',
+        uploadId: 'string',
       },
     ],
-    likes: 1,
-    date: '2 Hours ago',
+    createdAt: '2025-04-02T06:32:49.419Z',
+    updatedAt: '2025-04-02T06:32:49.419Z',
+    ownerId: 1,
+    avatarOwner: '/google.svg',
+    owner: {
+      firstName: 'firstName',
+      lastName: 'lastName',
+    },
+    likesCount: 1,
+    isLiked: true,
+    avatarWhoLikes: ['/google.svg', '/google.svg', '/google.svg', '/google.svg'],
+  },
+  {
+    id: 2,
+    userName: 'Alex',
+    description:
+      'description URLProfiele Lorem ipsum dolor sit amet, ' +
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+      'ut labore et dolore magna aliqua.description URLProfiele Lorem ipsum dolor sit amet, ' +
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+      'ut labore et dolore magna aliqua.description URLProfiele Lorem ipsum dolor sit amet, ' +
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+      'ut labore et dolore magna aliqua.description URLProfiele Lorem ipsum dolor sit amet, ' +
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+      'ut labore et dolore magna aliqua.description URLProfiele Lorem ipsum dolor sit amet, ' +
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ' +
+      'ut labore et dolore magna aliqua.',
+    location: 'location',
+    images: [
+      {
+        url: '/google.svg',
+        width: 300,
+        height: 300,
+        fileSize: 300,
+        createdAt: '2025-04-02T06:32:49.111Z',
+        uploadId: 'string',
+      },
+    ],
+    createdAt: '2025-04-02T06:32:49.419Z',
+    updatedAt: '2025-04-02T06:32:49.419Z',
+    ownerId: 1,
+    avatarOwner: '/google.svg',
+    owner: {
+      firstName: 'firstName',
+      lastName: 'lastName',
+    },
+    likesCount: 100,
+    isLiked: true,
+    avatarWhoLikes: ['/google.svg', '/google.svg', '/google.svg'],
   },
 ]
 
 const PostList = () => {
-  const posts = mockPosts // В реальном приложении здесь будет useGetPostsQuery
-  const [showAnswers, setShowAnswers] = useState<{ [key: number]: boolean }>({})
+  const posts = mockPosts
+  const [showAnswers, setShowAnswers] = useState<Record<number, boolean>>({})
+  const [comment, setComment] = useState('')
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
 
+  const handleClickOutside = (e: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+      setIsMenuOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+  const toggleMenu = () => setIsMenuOpen(prev => !prev)
+  const handleEditPost = () => {
+    console.log('Edit Post')
+    setIsMenuOpen(false)
+  }
+  const handleDeletePost = () => {
+    console.log('delete Post')
+    setIsMenuOpen(false)
+  }
+
+  const handleOptionsClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    toggleMenu()
+  }
   const toggleAnswers = (index: number) => {
     setShowAnswers(prev => ({
       ...prev,
@@ -43,65 +116,109 @@ const PostList = () => {
     }))
   }
 
+  const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value)
+  }
+
+  const handleCommentSubmit = () => {
+    // Здесь можно обработать отправку комментария
+    console.log('Comment submitted:', comment)
+    setComment('')
+  }
+
   if (!posts || posts.length === 0) return <p>No posts found</p>
 
   return (
-    <div className={s.postList}>
-      {posts.map(post => (
-        <div key={post.id} className={s.post}>
-          {/* Заголовок поста */}
-          <div className={s.header}>
-            <span>{post.username}</span>
-            {/* Кнопка с тремя точками будет добавлена позже */}
-          </div>
-
-          {/* Отрисовка поста */}
-          <Post
-            imageUrl={post.imageUrl}
-            description={post.description}
-            likes={post.likes}
-            date={post.date}
-          />
-
-          {/* Комментарии */}
-          <div className={s.comments}>
-            {post.comments.map((comment, index) => (
-              <div key={index} className={s.comment}>
-                <span>{comment.user}</span>
-                <p>{comment.text}</p>
-                <div className={s.commentMeta}>
-                  <span>{comment.timestamp}</span>
-                  {comment.answers && comment.answers.length > 0 && (
-                    <button onClick={() => toggleAnswers(index)} className={s.toggleAnswers}>
-                      {showAnswers[index] ? 'Hide Answers' : `Answer: ${comment.answers.length}`}
-                    </button>
-                  )}
-                </div>
-                {showAnswers[index] && comment.answers && (
-                  <div className={s.answers}>
-                    {comment.answers.map((answer, answerIndex) => (
-                      <div key={answerIndex} className={s.answer}>
-                        <span>{answer.user}</span>
-                        <p>{answer.text}</p>
-                        <span>{answer.timestamp}</span>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-
-          {/* Поле для добавления комментария */}
-          <div className={s.commentInputWrapper}>
-            <input type="text" placeholder="Answer" className={s.commentInput} />
-            <button className={s.commentSubmit}>
-              <Image src="/send.svg" alt="Send" width={20} height={20} />
+    <div className={s.postListWrapper}>
+      <div className={s.header}>
+        <div className={s.name}>
+          <Image src="/google.svg" alt="Post image" width={20} height={20} className={s.image} />
+          <h3>URLProfiele</h3>
+        </div>
+        <button onClick={handleOptionsClick}>...</button>
+        {isMenuOpen && (
+          <div ref={menuRef} className={s.dropdownMenu}>
+            <button onClick={handleEditPost}>
+              <Image src={'/edit-2-outline.svg'} alt={'edit'} width={24} height={24} />
+              Edit Post
+            </button>
+            <button onClick={handleDeletePost}>
+              <Image src={'/trash-outline.svg'} alt={'trash'} width={24} height={24} />
+              Delete Post
             </button>
           </div>
+        )}
+      </div>
+      <hr className={s.separator} />
+
+      <div className={s.scrollableContent}>
+        {posts.map(post => (
+          <Post
+            key={post.id}
+            id={post.id}
+            imageUrl={post.images[0]?.url || ''}
+            description={post.description}
+            likes={post.likesCount}
+            date={new Date(post.createdAt).toLocaleString()}
+            avatarOwner={post.avatarOwner}
+            owner={post.owner}
+          />
+        ))}
+      </div>
+
+      <hr className={s.separator} />
+
+      <div className={s.infoStories}>
+        <div className={s.infoStoriesIcone}>
+          <div>
+            <button>
+              <Image src="/heart-outline.svg" alt="heart" width={20} height={20} />
+            </button>
+            <button>
+              <Image src="/paper-plane-outline.svg" alt="plane" width={20} height={20} />
+            </button>
+          </div>
+          <button>
+            <Image src="/bookmark-outline.svg" alt="bookmar" width={20} height={20} />
+          </button>
         </div>
-      ))}
+        <div className={s.footerInfo}>
+          <div className={s.footerAva}>
+            <div>
+              {mockPosts[0].avatarWhoLikes &&
+                mockPosts[0].avatarWhoLikes.map((ava, index) => (
+                  <Image
+                    key={index}
+                    src={ava}
+                    alt="User avatar"
+                    width={20}
+                    height={20}
+                    className={s.avatarSmall}
+                  />
+                ))}
+            </div>
+            <div className={s.likeCount}>
+              {mockPosts[0].likesCount} <span>Like</span>
+            </div>
+          </div>
+        </div>
+        <div className={s.date}>{new Date(mockPosts[0].createdAt).toLocaleDateString()}</div>
+      </div>
+
+      <div className={s.commentInputWrapper}>
+        <input
+          type="text"
+          placeholder="Add a Comment..."
+          className={s.commentInput}
+          value={comment}
+          onChange={handleCommentChange}
+        />
+        <button className={s.commentSubmit} onClick={handleCommentSubmit}>
+          Publish
+        </button>
+      </div>
     </div>
   )
 }
+
 export default PostList
