b57f8671abb8dbe4ab984fe914a44fae29b83170
Vitaliy <egoshinvit@gmail.com>
2025-04-09 23:46:36 +0700

    refactor: add home page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,21 @@
 import { useGetTotalUsersCountQuery } from '@/lib/api/authApi'
 import { useGetPublicPostsQuery } from '@/lib/api/postApi'
 import s from './page.module.scss'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+import Link from 'next/link'
+import { PATH } from '@/shared/const/PATH'
+import { formatTimeAgo } from '@/shared/utils/formatTimeAgo'
 
 export default function Home() {
+  const screenWidth = window.innerWidth
+  const containerWidth = screenWidth * 0.75
+  const postsCount = Math.floor(containerWidth / 252)
   const { data } = useGetTotalUsersCountQuery()
-  const { data: postsData } = useGetPublicPostsQuery()
+  const { data: postsData } = useGetPublicPostsQuery(postsCount)
 
   const totalCount = data?.totalCount.toString().padStart(6, '0')
 
@@ -22,9 +33,32 @@ export default function Home() {
       </div>
       <div className={s.postsBlock}>
         {postsData?.items.map((post) => <div key={post.id} className={s.post}>
-          <div className={s.photo}>photo</div>
-          <div>block title</div>
-          <div>desc</div>
+          {post.images.length > 0 && (
+            <Swiper
+              modules={[Navigation, Pagination]}
+              navigation
+              pagination={{ clickable: true }}
+              className={s.carousel}
+            >
+              {post.images.map((image) => (
+                <SwiperSlide key={image.url}>
+                  <Link href={PATH.USERS.PROFILE_USERID(post.ownerId) + '?postId=' + post.id}>
+                    <img
+                      className={s.photo}
+                      src={image.url}
+                      alt="post photo"
+                    />
+                  </Link>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+          <div className={s.author}>
+            <img className={s.avatar} src={post.avatarOwner} alt="" />
+            <Link className={s.title} href={PATH.USERS.PROFILE_USERID(post.ownerId)}>URLProfile</Link>
+          </div>
+          <div className={s.time}>{formatTimeAgo(post.createdAt)}</div>
+          <p className={s.description}>{post.description}</p>
         </div>)}
       </div>
     </div>
