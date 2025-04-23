e115d72182841141b188e0681c28a3ddd798793f
Lena <lena3005@gmail.com>
2025-04-23 21:27:40 +0600

    add subscriptionApi

--- src/shared/api/subscriptionApi.ts
+++ src/shared/api/subscriptionApi.ts
@@ -1,3 +1,8 @@
+import type {
+  CreateSubscriptionInput,
+  CurrentSubscriptionResponse,
+  Payment,
+} from '@/shared/lib/types/subscriptionTypes'
 import { baseApi } from '@/store/services/baseApi'
 
 export const subscriptionApi = baseApi.injectEndpoints({
@@ -7,7 +12,38 @@ export const subscriptionApi = baseApi.injectEndpoints({
         url: 'subscriptions/canceled-auto-renewal',
         method: 'POST',
       }),
+      invalidatesTags: ['subscription'],
+    }),
+    getCurrentSubscription: build.query<CurrentSubscriptionResponse, void>({
+      query: () => 'subscriptions/current-payment-subscriptions',
+      providesTags: ['subscription'],
+    }),
+    createSubscription: build.mutation<{ url: string }, CreateSubscriptionInput>({
+      query: body => ({
+        url: 'subscriptions',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['subscription'],
+    }),
+    getMyPayments: build.query<Payment[], void>({
+      query: () => 'subscriptions/my-payments',
+      providesTags: ['subscription'],
+    }),
+    getSubscriptionCosts: build.query<
+      { data: { amount: number; typeDescription: string }[] },
+      void
+    >({
+      query: () => 'subscriptions/cost-of-payment-subscriptions',
+      providesTags: ['subscription'],
     }),
   }),
 })
-export const { useCancelAutoRenewalMutation } = subscriptionApi
+
+export const {
+  useCancelAutoRenewalMutation,
+  useGetCurrentSubscriptionQuery,
+  useCreateSubscriptionMutation,
+  useGetMyPaymentsQuery,
+  useGetSubscriptionCostsQuery,
+} = subscriptionApi
