30a12c7694eab5b1f43cf20f53bd8bfed7b209fd
Vitaliy <egoshinvit@gmail.com>
2025-04-26 08:35:27 +0700

    hotfix

--- src/app/users/profile/[userId]/page.tsx
+++ src/app/users/profile/[userId]/page.tsx
@@ -3,17 +3,14 @@ import UserProfile from '@/shared/ui/UserProfile/UserProfile'
 import type { PublicProfile } from '@/shared/lib/types/profileTypes'
 import { cookies } from 'next/headers'
 import { PostType } from '@/shared/lib/types/postsTypes'
-
+// @ts-ignore
 export default async function PublicUserProfilePage({
-                                                      params,
-                                                      searchParams,
+                                                      params: { userId },
+                                                      searchParams: { postId },
                                                     }: {
   params: { userId: string }
   searchParams: { postId?: string }
 }) {
-  const { userId } = params
-  const { postId } = searchParams
-
   try {
     const cookieStore = await cookies()
     const token = cookieStore.get('accessToken')?.value
