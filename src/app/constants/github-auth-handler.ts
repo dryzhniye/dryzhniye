4f25b210b429bf7fd193d8d9bb6da3a04aac5594
Vitaliy <egoshinvit@gmail.com>
2025-04-26 07:31:50 +0700

    hotfix

--- src/app/constants/github-auth-handler.ts
+++
@@ -1,4 +0,0 @@
-export const handleGithubAuth = () => {
-  const redirectUrl = 'https://dryzhniye.ru/auth/callback'
-  window.location.href = `https://dryzhniye.ru/api/v1/auth/github/login?redirect_url=${encodeURIComponent(redirectUrl)}`
-}
