0509e0ddc48d7ff3d7d533ac93f5d0d7f4d35378
Vitaliy <egoshinvit@gmail.com>
2025-04-27 09:06:29 +0700

    hotfix

--- src/shared/ui/base/TextArea/TextArea.stories.ts
+++ src/shared/ui/base/TextArea/TextArea.stories.ts
@@ -2,7 +2,6 @@ import {Button} from "@/shared/ui/base/Button/Button";
 import type {Meta, StoryObj} from "@storybook/react";
 import TextArea from "@/shared/ui/base/TextArea/TextArea";
 
-
 const meta = {
     component: TextArea,
 } satisfies Meta<typeof Button>
@@ -13,7 +12,6 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
     args: {
         title: 'Hello. I textarea',
-        error: true,
         disabled: false
     },
 }
